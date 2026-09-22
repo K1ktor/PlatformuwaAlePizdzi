3581402a7077b0302a91aa77b2428e9ffe5183d1
ErdenArt <76409165+ErdenArt@users.noreply.github.com>
2026-09-22 19:37:07 +0200

    Player object

--- Frost Platform Game/Player.gd
+++ Frost Platform Game/Player.gd
@@ -1,4 +1,8 @@
-extends Sprite2D
+extends CharacterBody2D
+
+@export var speed := 600.0
+@export var jump_strength := 1500.0
+@export var gravity := 4500.0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +12,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var _horizontal_direction = (
+		Input.get_action_strength("moveRight")
+		- Input.get_action_strength("moveLeft")
+	)
+	if (is_on_floor() and Input.is_action_pressed("jump")):
+		velocity.y -= jump_strength
+	velocity.x = _horizontal_direction * speed
+	velocity.y += gravity * delta
+	
+	move_and_slide()
+	
 	pass
